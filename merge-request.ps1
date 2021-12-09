188a486d0792d9561bca19e5866dfeb4b5691d74
62274677 <36176438+62274677@users.noreply.github.com>
2021-12-09 03:04:20 -0800

    Update merge-requests.ps1 to include comments

--- merge-request.ps1
+++ merge-request.ps1
@@ -1,14 +1,14 @@
 #  TODO - automated log cleanup and backup
 
 $fso = New-Object -com "Scripting.FileSystemObject"
-$gitlab_path = "C:\EDIT\THIS\PATH"
+$gitlab_path = "Z:\Downloads\Chromium\New folder (2)"
 $logfile = "$gitlab_path\gitlab-merge-request-cleanup.log"
 
-If (-not (Test-Path -Path $gitlab_path)){
-    $logfile = "C:\Temp\gitlab-merge-request-cleanup.log"
+If (-not (Test-Path -Path $gitlab_path)){ # If hard-coded gitlab folder does not exist
+    $logfile = "C:\Temp\gitlab-merge-request-cleanup.log" # create cleanup.log in C:/Temp directory
 }
 
-Function log-write
+Function log-write # Function to write logs to file (can be further standardized)
 {
    Param ([string]$logstring)
 
@@ -19,20 +19,24 @@ Function log-write
    Add-content $Logfile -value $line
 }
 
-If (Test-Path -Path $gitlab_path){
+If (Test-Path -Path $gitlab_path){ # If Gitlab path exists
     $folder = $fso.GetFolder($gitlab_path)
     $deleted = 0
-    foreach ($subfolder in $folder.SubFolders){
-        If ($subfolder.Name -match ".+_MR-\d+-.+"){ #.+_MR-\d{2,}-.+
+    foreach ($subfolder in $folder.SubFolders){ # For each folder in the hardcoded root directory:
+        If ($subfolder.Name -match ".+_MR-\d+-.+"){ #.+_MR-\d{2,}-.+ 
+                                                    # If the folder name matches a Merge Request:
+
             $deleted = 1
             $folder_path = convert-path $subfolder.Path
             $items = Get-ChildItem $subfolder.Path -Recurse 
-            $most_recent_file = $items | Sort-Object LastWriteTime -Descending | Select-Object -First 1
-            IF(($most_recent_file).LastWriteTime -lt  (Get-Date).AddMinutes(-30)){
+            $most_recent_file = $items | Sort-Object LastWriteTime -Descending | Select-Object -First 1 
+                                                            # Get the most recently edited file in the subdirectory
+            If(($most_recent_file).LastWriteTime -lt  (Get-Date).AddMinutes(-30)){  # If the most recent edit is older than 30 minutes:
+                                                                                    # Delete all files in current subdirectory
                 remove-item $subfolder.Path -Verbose -Recurse -Force
                 log-write "Removing folder [$folder_path] and all subfiles and folders." 
             }
-            Else{
+            Else{ # Else if merge requests found with files younger than 30 minutes, do nothing
                 log-write "Merge Request found younger than 30 minutes. Skipping folders and files."
             }
             
@@ -40,15 +44,16 @@ If (Test-Path -Path $gitlab_path){
             # Get-ChildItem -Path $subfolder.Path -Include *.* -File -Recurse | foreach { $_.Delete()}
             # remove-item $subfolder.Path -Verbose -Recurse -Force
             
-        }       
+            
+        }  # Repeat
     }
-    If (-not($deleted)){
+    If (-not($deleted)){ # If the script doesn't find any files to delete, report this in the log file
         log-write "GitLab Merge Request Cleanup has been run. No folders to clean up have been found."
     }
-    Else{
+    Else{ # If the script has found files to delete, report that it has completed.
         log-write "GitLab Merge Request Cleanup has completed."
     }
 }
-Else {
+Else { # If the script could not find the gitlab merge folder, output to C:/Temp this message
     log-write "Gitlab Folder not found. Please check filepath. Expecting: [$gitlab_path]."
 }
